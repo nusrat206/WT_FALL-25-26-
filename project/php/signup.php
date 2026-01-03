df31e05b49573ae707d608fa81b3d5cdab722042
nusrat206 <nusratsultanakhan37@gmail.com>
2026-01-04 05:42:14 +0600

    adding email in form sighup

--- project/php/signup.php
+++ project/php/signup.php
@@ -10,6 +10,9 @@
 
             <label for="age">age:</label>
             <input type="number" name ="age" value="<?php echo htmlspecialchars($age); ?>">required>
+
+            <label for="email">email:</label>
+            <input type="email" name ="email" value="<?php echo htmlspecialchars($email); ?>">required>
         </form>
     </body>
 </html>
