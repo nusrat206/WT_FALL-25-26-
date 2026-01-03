ad022899d750e48f536da5a3b8e8a634c6be1a27
nusrat206 <nusratsultanakhan37@gmail.com>
2026-01-04 05:39:54 +0600

    adding name in signup with php

--- project/php/signup.php
+++ project/php/signup.php
@@ -4,6 +4,9 @@
         <title>Sign up</title>
     </head>
     <body>
-        <form action="login.php"></form>
+        <form action="login.php" method ="post">
+            <label for="name">Name:</label>
+            <input type="text" name ="name" value="<?php echo htmlspecialchars($name); ?>">required>
+        </form>
     </body>
 </html>
