749aaf86740275e028e2f3cdd5c93b9415ccdf74
nusrat206 <nusratsultanakhan37@gmail.com>
2026-01-04 06:31:46 +0600

    connecing database through db.php

---
+++ project/db/db.php
@@ -0,0 +1,14 @@
+<?php
+$host="localhost"
+$user="root"
+$pass=""
+$dbname="auto"
+
+$conn=new mysqli($host,$user,$pass,$dbname);
+
+if($conn->connect_error)
+{
+    die("connection failed: ".$conn->connection_error);
+
+}
+?>
